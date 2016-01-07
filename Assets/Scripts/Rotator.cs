bcb7727d1b0ee60f65a7e66ba7047483749f9184
yushroom <yushroom@gmail.com>
2016-01-07 22:45:46 +0800

    add models and texture for StartMenu add Destroy target

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -6,6 +6,7 @@ public class Rotator : MonoBehaviour {
 	//public Vector3 axis = new Vector3(0, 1, 0);
 	public float speed = 1.0f;
 	bool _rotating;
+    public bool auto = false;
 
 	// Use this for initialization
 	void Start () {
@@ -14,7 +15,8 @@ public class Rotator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (_rotating) {
+		if (auto) {
+            rotate();
 		}
 	}
 
